c694edca62a3554b17c4023d0942957b05b2156c
Justin Young <45580031+Juatin8@users.noreply.github.com>
2023-07-23 10:30:54 +0800

    Update and rename LongClickToBuild.cs to LongClickToDo.cs

--- 用户输入逻辑/LongClickToDo.cs
+++ 用户输入逻辑/LongClickToDo.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class LongClickToBuild : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class LongClickToDo : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     private float downTime = 0;
     private bool isDragging = false;
+    private float timelimit =0.8f;
 
     void Update()
     {
         if(isDragging){
-            // 长按两秒
-            if(Time.time - downTime > 0.8f)
+            // 长按xx秒
+            if(Time.time - downTime > timelimit)
             {
                 isDragging = false;
                 // 你要处理的逻辑
