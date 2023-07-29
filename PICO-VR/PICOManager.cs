efae08b2b70c85bad4cd5540c1b839b26be4afc6
Justin Young <45580031+Juatin8@users.noreply.github.com>
2023-07-29 13:45:25 +0800

    Add files via upload

---
+++ PICO-VR/PICOManager.cs
@@ -0,0 +1,58 @@
+
+using Unity.XR.PXR;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public static class PICOManager
+{
+    static InputDevice RHand = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+    static InputDevice LHand = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+    static InputDevice Head = InputDevices.GetDeviceAtXRNode(XRNode.Head);
+
+    static bool isHeadOn;
+
+    //--------------------------读PICO设备------------------------------
+
+    public static void getAccelaration()
+    {
+        float acceleration = Input.GetAxis("Acceleration");
+    }
+
+    public static void back2SystemMenu()
+    {
+        bool isdown;
+        if(RHand.TryGetFeatureValue(CommonUsages.primaryButton, out isdown) && isdown)
+        {
+            SceneManager.LoadScene("EnterUI", LoadSceneMode.Single);
+        }
+    }
+
+    //-------------------------写PICO设备-------------------------
+    public static void VolumeDown()
+    {
+        PXR_System.VolumeDown();
+    }
+
+    public static void VolumeUP()
+    {
+        PXR_System.VolumeUp();
+    }
+
+
+    public static class VibrationController
+    {
+        public static void HandVibrate(float intensity, int duration, int hand)     //简化封装手柄震动的函数
+        {
+            if(hand == 0)
+            {
+                PXR_Input.SetControllerVibration(intensity, duration, PXR_Input.Controller.LeftController);
+            }
+            if(hand == 1)
+            {
+                PXR_Input.SetControllerVibration(intensity, duration, PXR_Input.Controller.RightController);
+            }
+        }
+    }
+
+}
