caf33494f8ace21f32eec06f89494c3cd582268a
Justin Young <45580031+Juatin8@users.noreply.github.com>
2023-08-24 18:58:33 +0800

    Add files via upload

---
+++ 图片处理/ImageManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageManager : MonoBehaviour
+{
+    public static void ChangeAlpha(GameObject gb, float alpha)   //改变图片透明度
+    {
+        if(alpha < 0) { alpha = 0; }
+        if(alpha > 1) { alpha = 1; }
+        gb.GetComponent<Image>().color = new Color(255, 255, 255, alpha);
+    }
+}
