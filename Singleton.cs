1b08a8b61f8efc0c0fc7e3763dde0ec0bd49c46f
Justin Young <45580031+Juatin8@users.noreply.github.com>
2024-10-07 11:21:51 +0800

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                // 尝试查找现有的实例
+                _instance = FindObjectOfType<T>();
+
+                // 如果没有找到，创建一个新的 GameObject 并添加实例
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(T).Name);
+                    _instance = singletonObject.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        // 保证 Singleton 的唯一性
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
